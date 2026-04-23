355b2536135defa19c83027d32c3a80136a47d31
Dominic Gannaway <dg@domgan.com>
2026-04-23 16:10:52 +0100

    fix volar mappings

--- packages/tsrx/src/transform/jsx/helpers.js
+++ packages/tsrx/src/transform/jsx/helpers.js
@@ -104,6 +104,7 @@ export function tsx_with_ts_locations() {
 		// JS nodes whose esrap printer emits no location marker, causing
 		// segments.js get_mapping_from_node() to throw when it asks for the
 		// generated position of the node's start (or end).
+		'IfStatement',
 		'NewExpression',
 		'MemberExpression',
 		'ObjectExpression',

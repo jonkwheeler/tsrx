9b41828b6bd2e049eb7d724456d0409f651f1931
Jon K. Wheeler <jon.k.wheeler@gmail.com>
2026-08-31 14:29:56 -0400

    feat(intellij): build marketplace-ready archive

--- packages/intellij-plugin/build.gradle.kts
+++ packages/intellij-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	id("java")
 	id("org.jetbrains.kotlin.jvm") version "2.1.20"
-	id("org.jetbrains.intellij.platform") version "2.10.2"
+	id("org.jetbrains.intellij.platform") version "2.18.1"
 }
 
 group = "dev.tsrx.intellij_plugin"
@@ -16,6 +16,8 @@ repositories {
 
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin.html
 dependencies {
+	testImplementation("junit:junit:4.13.2")
+
 	intellijPlatform {
 		webstorm("2025.2.4")
 		testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
@@ -28,16 +30,42 @@ dependencies {
 intellijPlatform {
 	pluginConfiguration {
 		ideaVersion {
-			sinceBuild = "252.25557"
+			sinceBuild = "252"
 		}
 
 		changeNotes = """
-	            TSRX language support for IntelliJ Platform IDEs.
-	        """.trimIndent()
+			<p>TSRX language support for IntelliJ Platform IDEs.</p>
+			<ul>
+				<li>TextMate syntax highlighting and baseline editor support.</li>
+				<li>Optional language-server integration in supported JetBrains IDEs.</li>
+			</ul>
+		""".trimIndent()
 	}
 }
 
+val generatedPluginResources = layout.buildDirectory.dir("generated/tsrx-plugin-resources")
+val generatePluginResources by tasks.registering(Sync::class) {
+	from(layout.projectDirectory.file("../../grammars/textmate/info.plist")) {
+		into("textmate")
+	}
+	from(layout.projectDirectory.file("../../grammars/textmate/tsrx.tmLanguage.json")) {
+		into("textmate/Syntaxes")
+	}
+	from(layout.projectDirectory.file("LICENSE")) {
+		into("META-INF")
+	}
+	into(generatedPluginResources)
+}
+
+sourceSets.main {
+	resources.srcDir(generatedPluginResources)
+}
+
 tasks {
+	processResources {
+		dependsOn(generatePluginResources)
+	}
+
 	// Set the JVM compatibility versions
 	withType<JavaCompile> {
 		sourceCompatibility = "21"
